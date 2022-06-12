290f176e8126c0d27953802159a2d53c8ea7741e
maqsudjon <m.khusainov@temida.io>
2022-06-12 21:44:28 +0200

    implement request validation

--- app/Http/Controllers/CurrencyConverterController.php
+++ app/Http/Controllers/CurrencyConverterController.php
@@ -6,6 +6,7 @@ use App\DataProviders\Contracts\CurrencyProviderInterface;
 use App\Entities\CurrencyEntity;
 use App\ResponseFormatters\Contracts\CurrencyResponseHandlerInterface;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class CurrencyConverterController extends Controller
 {
@@ -18,8 +19,21 @@ class CurrencyConverterController extends Controller
     {
     }
 
-    public function convert(Request $request)
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function convert(Request $request): Response
     {
+        $this->validate($request, [
+            'baseCurrencyCode' => 'required|string',
+            'amount' => 'required|numeric',
+            'currencies' => 'required|array|max:10',
+            'currencies.*' => 'string|distinct',
+        ]);
+
         $currency = new CurrencyEntity($request->get('baseCurrencyCode', ''), $request->get('amount', .0), new \DateTime());
 
         $currencies = array_map(fn ($value) => new CurrencyEntity($value, .0, new \DateTime()), $request->get('currencies', []));
